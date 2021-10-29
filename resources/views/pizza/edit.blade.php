77f15dfae0f073d826b9a2da7da27c7fa81270eb
Sani Kamal <sanikamal223@gmail.com>
2021-10-29 19:01:03 +0530

    edit and delete features added

--- resources/views/pizza/edit.blade.php
+++ resources/views/pizza/edit.blade.php
@@ -3,18 +3,7 @@
 @section('content')
     <div class="container-fluid">
         <div class="row justify-content-center">
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-header">{{ __('Menu') }}</div>
-
-                    <div class="card-body">
-                        <ul class="list-group">
-                          <a href="{{route('pizza.index')}}"  class="list-group-item list-group-item-action">View</a>
-                            <a href="{{route('pizza.create')}}" class="list-group-item list-group-item-action">Add</a>
-                        </ul>
-                    </div>
-                </div>
-
+            <div class="col-md-8">
                 @if (count($errors) > 0)
                     <div class="card mt-5">
                         <div class="card-body">
@@ -26,32 +15,31 @@
                         </div>
                     </div>
                 @endif
-            </div>
-            <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Pizza</div>
-                    <form action="{{route('pizza.store')}}" method="post" enctype="multipart/form-data">
+                    <div class="card-header">Edit Pizza</div>
+                    <form action="{{route('pizza.update',$pizza->id)}}" method="post" enctype="multipart/form-data">
                         @csrf
+                        @method('PUT')
                         <div class="card-body">
                             <div class="form-group">
                                 <label for="name">Name of Pizza</label>
-                                <input type="text" class="form-control" name="name" id="name" placeholder="Enter Pizza Name"/>
+                                <input type="text" class="form-control" name="name" id="name" placeholder="Enter Pizza Name" value="{{$pizza->name}}"/>
 
                             </div>
                             <div class="form-group">
                                 <label for="description">Description of Pizza</label>
                                 <textarea type="text" class="form-control" name="description"
-                                          id="description"> </textarea>
+                                          id="description">{{$pizza->description}} </textarea>
 
                             </div>
                             <div class="form-inline">
                                 <label>Pizza Price(₹)</label>
                                 <input type="number" class="form-control" name="small_pizza_price"
-                                       placeholder="Small Pizza Price"/>
+                                       placeholder="Small Pizza Price" value="{{$pizza->small_pizza_price}}"/>
                                 <input type="number" class="form-control" name="medium_pizza_price"
-                                       placeholder="Medium Pizza Price"/>
+                                       placeholder="Medium Pizza Price" value="{{$pizza->medium_pizza_price}}"/>
                                 <input type="number" class="form-control" name="large_pizza_price"
-                                       placeholder="Large Pizza Price"/>
+                                       placeholder="Large Pizza Price" value="{{$pizza->medium_pizza_price}}"/>
 
 
                             </div>
@@ -69,10 +57,10 @@
                             <div class="form-group">
                                 <label for="image">Image of Pizza</label>
                                 <input type="file" class="form-control" name="image"/>
-
+                                <img src="{{ Storage::url($pizza->image) }}" width="80">
                             </div>
                             <div class="form-group text-center">
-                                <button class="btn btn-primary" type="submit">Save</button>
+                                <button class="btn btn-primary" type="submit">Update</button>
 
                             </div>
                         </div>
